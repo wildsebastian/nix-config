f7a296be42ae1310f4b2295f5ab562cd22d88505
Sebastian Wild <sebastian@wild-siena.com>
2018-08-21 00:21:13 +0200

    Remove MacPorts Emacs

--- emacs.nix
+++ emacs.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  myEmacs = pkgs.emacs25Macport;
+  myEmacs = pkgs.emacs;
   emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
 in
   emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
