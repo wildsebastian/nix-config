0d89e3511725991a92e70fd9938573cc13a2ba61
Sebastian Wild <sebastian@wild-siena.com>
2019-12-07 12:16:48 +0100

    Fix nano override

--- overlays/nano.nix
+++ overlays/nano.nix
@@ -1,9 +1,11 @@
 self: super:
 {
-  nano = super.nano.overrideDerivation(old: {
-    src = super.fetchurl {
-      url = "https://git.savannah.gnu.org/cgit/nano.git/snapshot/nano-f517cddce749c3bf938271ef3182b9169ac8cbcc.tar.gz";
-      sha256 = "1s98jsvkfar6qmd5n5l1n1k59623dnc93ciyvlhxjkvpad0kmb4v";
+  nano = super.nano.overrideAttrs (old: rec {
+    version = "4.6-dev";
+    src = super.fetchgit {
+      url = "https://git.savannah.gnu.org/git/nano.git";
+      rev = "f516cddce749c3bf938271ef3182b9169ac8cbcc";
+      sha256 = "1dahpkiw406whkanm8p55zlj3qdnhbqjpss68fl983xcg6q0qzyh";
     };
   });
 }
