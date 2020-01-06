e44373280c5b759fe3a1cf8f49bb3ab385bc5a8d
Sebastian Wild <sebastian@wild-siena.com>
2020-01-06 17:52:22 +0100

    Update haskell overrides

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -5,32 +5,18 @@ self: pkgs:
     packages = pkgs.haskell.packages // {
       ghc865 = pkgs.haskell.packages.ghc865.override {
         overrides = self: super: with pkgs.haskell.lib; {
-          HaskellNet        = doJailbreak super.HaskellNet;
-          QuickCheck        = dontCheck super.QuickCheck;
-          Glob              = dontCheck super.Glob;
-          cron              = dontCheck super.cron;
-          comonad           = dontCheck super.comonad;
-          hakyll            = dontCheck (doJailbreak super.hakyll);
-          http-types        = dontCheck super.http-types;
-          lens              = dontCheck super.lens;
-          pdf-toolbox-content = self.callCabal2nix "pdf-toolbox-content" (pkgs.fetchgit {
-              url = "https://github.com/Yuras/pdf-toolbox.git";
-              rev = "67ff8877c5445172072ef7b96ae6edc3ee06ee5e";
-              sha256 = "15yj3dk4rywhwksizxbnvrnvin7my0kjp25050sjwdf3mn50mjmn";
-            } + "/content") {};
-          pdf-toolbox-core  = self.callCabal2nix "pdf-toolbox-core" (pkgs.fetchgit {
-              url = "https://github.com/Yuras/pdf-toolbox.git";
-              rev = "67ff8877c5445172072ef7b96ae6edc3ee06ee5e";
-              sha256 = "15yj3dk4rywhwksizxbnvrnvin7my0kjp25050sjwdf3mn50mjmn";
-            } + "/core") {};
-          pdf-toolbox-document  = self.callCabal2nix "pdf-toolbox-document" (pkgs.fetchgit {
-              url = "https://github.com/Yuras/pdf-toolbox.git";
-              rev = "67ff8877c5445172072ef7b96ae6edc3ee06ee5e";
-              sha256 = "15yj3dk4rywhwksizxbnvrnvin7my0kjp25050sjwdf3mn50mjmn";
-            } + "/document") {};
-          semigroupoids     = dontCheck super.semigroupoids;
-          servant           = dontCheck super.servant;
-          time-compat       = dontCheck super.time-compat;
+          HaskellNet          = doJailbreak super.HaskellNet;
+          QuickCheck          = dontCheck super.QuickCheck;
+          Glob                = dontCheck super.Glob;
+          cron                = dontCheck super.cron;
+          comonad             = dontCheck super.comonad;
+          hakyll              = dontCheck (doJailbreak super.hakyll);
+          http-types          = dontCheck super.http-types;
+          lens                = dontCheck super.lens;
+          semigroupoids       = dontCheck super.semigroupoids;
+          servant             = dontCheck super.servant;
+          servant-auth-server = doJailbreak super.servant-auth-server;
+          time-compat         = dontCheck super.time-compat;
         };
       };
 
