c943a80e1fbdbf88b3516a3c800c56659f9f740b
Sebastian Wild <sebastian@wild-siena.com>
2020-03-01 09:06:46 +0100

    Update haskell overlays for ghcjs

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -5,19 +5,20 @@ self: pkgs:
     packages = pkgs.haskell.packages // {
       ghc865 = pkgs.haskell.packages.ghc865.override {
         overrides = self: super: with pkgs.haskell.lib; {
-          HaskellNet          = doJailbreak super.HaskellNet;
-          QuickCheck          = dontCheck super.QuickCheck;
-          Glob                = dontCheck super.Glob;
-          cron                = dontCheck super.cron;
-          comonad             = dontCheck super.comonad;
-          hakyll              = dontCheck (doJailbreak super.hakyll);
-          http-types          = dontCheck super.http-types;
-          lens                = dontCheck super.lens;
-          semigroupoids       = dontCheck super.semigroupoids;
-          servant             = dontCheck super.servant;
-          servant-auth-server = doJailbreak super.servant-auth-server;
-          servant-server      = doJailbreak super.servant-server;
-          time-compat         = dontCheck super.time-compat;
+          HaskellNet            = doJailbreak super.HaskellNet;
+          QuickCheck            = dontCheck super.QuickCheck;
+          Glob                  = dontCheck super.Glob;
+          cron                  = dontCheck super.cron;
+          comonad               = dontCheck super.comonad;
+          haddock-library-ghcjs = dontCheck super.haddock-library-ghcjs;
+          hakyll                = dontCheck (doJailbreak super.hakyll);
+          http-types            = dontCheck super.http-types;
+          lens                  = dontCheck super.lens;
+          semigroupoids         = dontCheck super.semigroupoids;
+          servant               = dontCheck super.servant;
+          servant-auth-server   = doJailbreak super.servant-auth-server;
+          servant-server        = doJailbreak super.servant-server;
+          time-compat           = dontCheck super.time-compat;
         };
       };
     };
