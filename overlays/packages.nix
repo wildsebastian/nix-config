aea1f967fbe7c8eb7efa1900b415664054ebd5fd
Sebastian Wild <sebastian@wild-siena.com>
2021-11-04 17:09:22 +0100

    Update package overlay

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -4,21 +4,6 @@ self: super:
     buildInputs = attrs.buildInputs ++ [ super.libiconv ];
   });
 
-  aws-sdk-cpp = super.aws-sdk-cpp.overrideDerivation (attrs: {
-    src = super.fetchFromGitHub {
-      owner = "awslabs";
-      repo = "aws-sdk-cpp";
-      rev = "1.8.130";
-      sha256 = "sha256-5T4l0KYB0utFTdEOtYT9trQ/JehQbXxk/IhI6YavErs=";
-    };
-    buildInputs = with self.pkgs; [
-      curl openssl zlib
-    ] ++ lib.optionals (stdenv.isDarwin &&
-                        ((builtins.elem "text-to-speech" ["*"]) ||
-                         (builtins.elem "*" ["*"])))
-            [ darwin.apple_sdk.frameworks.CoreAudio darwin.apple_sdk.frameworks.AudioToolbox ];
-  });
-
   proj = super.proj.overrideDerivation (attrs: {
     doCheck = false;
   });
