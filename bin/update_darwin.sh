583f4c94924077801cc8db993f962e4eb695f6a9
Sebastian Wild <sebastian@wild-siena.com>
2023-01-15 18:33:11 +0100

    GC after system update

--- bin/update_darwin.sh
+++ bin/update_darwin.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 cd ~/.nixpkgs
-
 git stash
-
 nix flake update
-
 darwin-rebuild switch --flake .#
-
 git add -u
-
 git commit -m "Update flake inputs"
-
 git push
-
 git stash pop
+sudo nix-collect-garbage --delete-older-than 7d
+cd ~/
