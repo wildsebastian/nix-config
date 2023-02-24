5b92084ce575b533720231f3e6c00f33de0ac892
Sebastian Wild <sebastian@wild-siena.com>
2023-02-24 11:47:01 +0100

    Update flake inputs and fix emacsql
     - Add sqlite to emacsql package override

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -8,6 +8,22 @@ pkgs.emacsWithPackagesFromUsePackage {
 
   extraEmacsPackages = epkgs: [
     epkgs.agda2-mode
+    (epkgs.emacsql.overrideAttrs (old: {
+      buildInputs = old.buildInputs ++ [ pkgs.sqlite ];
+
+      postBuild = ''
+        cd source/sqlite
+        make
+        cd -
+      '';
+
+      postInstall = (old.postInstall or "") + "\n" + ''
+        install -m=755 -D source/sqlite/emacsql-sqlite \
+          $out/share/emacs/site-lisp/elpa/emacsql-${old.version}/sqlite/emacsql-sqlite
+      '';
+
+      stripDebugList = [ "share" ];
+    }))
     epkgs.idris2-mode
   ];
 }
