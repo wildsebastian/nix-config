4a39d168d661a1adf78be6db1ea3de8eb6b0e21d
Sebastian Wild <sebastian@wild-siena.com>
2023-02-25 10:08:43 +0100

    Update flake inputs and Zotero

--- darwin/zotero.nix
+++ darwin/zotero.nix
@@ -3,12 +3,12 @@
 stdenv.mkDerivation rec {
   pname = "Zotero";
   # https://www.zotero.org/support/changelog
-  version = "6.0.21";
+  version = "6.0.22";
 
   src = fetchurl rec {
     name = "${pname}-${version}.dmg";
     url = "https://download.zotero.org/client/release/${version}/${name}";
-    sha256 = "sha256-U+fEk4/lzmk+EV2CUImxj0FvIoiJCNethFBDB0C6N9Y=";
+    sha256 = "sha256-9vDv7fwmGBtDcVyJ88xr/SOO4v2jYpMnzkUFw+V0wY0=";
   };
 
   buildInputs = [ undmg ];
