54c285299ad5a3a1a69aba42e7d3947df5ff28ea
Sebastian Wild <sebastian@wild-siena.com>
2023-03-23 08:08:39 +0100

    Update flake inputs and Firefox
    Firefox: 111.0 -> 111.0.1

--- darwin/firefox.nix
+++ darwin/firefox.nix
@@ -3,12 +3,12 @@
 stdenv.mkDerivation rec {
   pname = "Firefox";
   # https://product-details.mozilla.org/1.0/firefox_versions.json
-  version = "111.0";
+  version = "111.0.1";
 
   src = fetchurl rec {
     name = "${pname}-${version}.dmg";
     url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/${pname}%20${version}.dmg";
-    sha256 = "sha256-Fm89D+OyOxLsNBlR7DIxbpkdJ9Col9fYyVDH4VX+vxE=";
+    sha256 = "sha256-IeDeWL3rbovlwaQ6Ouod2SnhZGOMFyz6ucMWJwO9pjs=";
   };
 
   buildInputs = [ undmg ];
