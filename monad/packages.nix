99d1b9b414ee468e2c05faa1f21ba4c0c219b6a3
Sebastian Wild <sebastian@wild-siena.com>
2024-04-06 19:20:01 +0200

    Cleanup unused packages and update flake.lock

--- monad/packages.nix
+++ monad/packages.nix
@@ -6,57 +6,23 @@ in
 with pkgs; [
   pam-reattach
 
-  (agda.withPackages (p: [
-    p.standard-library
-  ]))
-  (import ../modules/emacs/emacs.nix { inherit pkgs; })
+  # (import ../modules/emacs/emacs.nix { inherit pkgs; })
 
-  diffstat
-  diffutils
-  gitRepo
-  gitAndTools.git-imerge
-  gitAndTools.gitflow
-  gitstats
   nix-prefetch-git
-  mercurialFull
-  patch
-  patchutils
 
-  asitop
-  autossh
-  (aspellWithDicts (d: [ d.de d.en d.it ]))
   bat
   bottom
-  cabal2nix
-  cachix
-  curl
-  ctags
   delta
   du-dust
   editorconfig-core-c
-  exercism
   eza
   fd
   fzf
-  git-credential-manager
-  gnugrep
   gnupg
-  htop
-  imgcat
   jq
-  minify
-  mosh
-  neofetch
-  ngrok
   nixos-rebuild
-  nushell
   openssl
-  procs
-  python39
   ripgrep
-  rustup
-  sd
-  silver-searcher
   tokei
   wakatime
   zotero
